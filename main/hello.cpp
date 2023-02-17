406e636150cfc8a653e48f864919e26955fcda0e
pranavskumar <125339530+pranavskumar@users.noreply.github.com>
2023-02-17 10:36:12 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Pranav S Kumar\n";
+  cout << "PES1UG20CS295\n";
+  cout << "Jenkins Assignment\n";
   return 0;
 }
